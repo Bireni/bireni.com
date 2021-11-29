477b166fe6b006eaa2e54f56afe776bc079707d9
bireni <abdelghanielmaanaoui@gmail.com>
2021-11-28 16:47:42 -0800

    add all files

---
+++ PHP/PHP.php
@@ -0,0 +1,23 @@
+<?php
+
+$ba = 1000;
+$bp = 1001;
+
+$bo = "I love PHP";
+$bi = TRUE;
+echo $bo;
+
+
+echo gettype($bo)."<br/>";
+
+var_dump($bo); echo "<br/>";
+
+
+var_dump($ba); echo "$ba"."<br/>";
+
+var_dump($bo); echo "$ba"."<br/>";
+
+
+ 
+
+?>
